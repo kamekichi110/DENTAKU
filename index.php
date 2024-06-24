c74ff26ab84be1b0bc06fee016dc6771401a1d01
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-06-24 09:43:24 +0000

    set

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ぱにこれ！ - official Website</title>
-  <link rel="stylesheet" href=".">
+  <link rel="stylesheet" href="./lib/onsen-ui/css/onsenui.min.css">
+  <link rel="stylesheet" href="./lib/onsen-ui/css/onsen-css-components.min.css">
+  <link rel="stylesheet" href="./css/theme.css">
+  <link rel="shortcut icon" href="./assets/img/icons/favicon.ico" type="image/x-icon">
+  <link rel="manifest" href="./app.json">
+  <script src="./lib/load-json/main.js"></script>
+  <script src="./lib/onsen-ui/js/onsenui.min.js"></script>
 </head>
 <body>
-  
+  <ons-navigator id="navigator" page="index.html"></ons-navigator>
+  <template id="index.html">
+    <ons-page>
+    </ons-page>
+  </template>
 </body>
 </html>
