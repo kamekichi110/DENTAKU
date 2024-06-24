2275cd52436ed68bc0ec5a09f0346c3f7d2e374f
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-06-24 09:31:54 +0000

    set

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<l>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ぱにこれ！ - official Website</title>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+  
+</body>
+</html>
